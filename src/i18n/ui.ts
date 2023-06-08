b267f6e20f25c712344287a8826472630f2c4ded
slavomirprochazka <prochazka.slavomir@prosoftke.sk>
2023-06-08 15:47:43 +0200

    language support

---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+  sk: 'Slovenčina',
+  cs: 'Čeština',
+  en: 'English',
+};
+
+export const defaultLang = 'sk';
+
+export const ui = {
+  sk: {
+    'nav.home': 'Domov',
+    'nav.about': 'About',
+    'nav.twitter': 'Twitter',
+  },
+  cs: {
+    'nav.home': 'Domů',
+    'nav.about': 'About',
+    'nav.twitter': 'Twitter',
+  },
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.twitter': 'Twitter',
+  },
+} as const;
